4b39d204f2d8e8793d351be4d78deb9b73138787
pluginsdb-create <plugins.db@gmail.com>
2026-04-17 20:48:36 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,8 @@ export default defineConfig(({mode}) => {
           theme_color: '#000000',
           background_color: '#000000',
           display: 'standalone',
+          start_url: '/',
+          scope: '/',
           icons: [
             {
               src: 'icon192x192.png',
